d38b04b2b0db60e79f521c2fec7b9a9beff60a5e
DualyRF <dualyrf@gmail.com>
2026-02-09 22:47:21 -0700

    Version 1 - Tarea2 - Linux

--- Scripts/Linux/Tarea2/tarea2_auto_DHCP.sh
+++ Scripts/Linux/Tarea2/tarea2_auto_DHCP.sh
@@ -15,63 +15,105 @@ gateway=""
 dns=""
 
 #Funciones
-ip_Correcta(){
-	local ip1=0
-	local ip2="$1"
-	if [[ ! "$ip2" =~ ^[0-9]+\.+[0-9]+\.[0-9]+\.[0-9]+$ ]] then
-		echo -e "${rojo}Direccion IP invalida, tiene que contener un formato X.X.X.X unicamente con numeros positivos${nc}"
-		return 1
-	fi	
+validar_IP() {
+    	local ip="$1"
+	
+    	# Validar formato X.X.X.X solo con numeros
+    	if ! [[ "$ip" =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+    	    echo -e "${rojo}Direccion IP invalida, tiene que contener un formato X.X.X.X unicamente con numeros positivos${nc}"
+        	return 1
+   	fi
 
-	for i in {1..4}; do
-		ip1=$(echo "$ip2" | cut -d'.' -f1)
-		ip2=${ip2#*.}
-		if [[ "$ip1" -gt 255 || "$ip1" -lt 0 ]]; then
-			echo -e "${rojo}Direccion IP invalida, no puede ser mayor a 255 ni menor a 0${nc}"
-			return 1
-		fi
-	done
-	return 0
+	# Validar que no sea 0.0.0.0 ni 255.255.255.255
+	if [ "$ip" = "0.0.0.0" || "$ip" = "255.255.255.255"]; then
+		echo -e "${rojo}Direccion IP invalida, no puede ser 0.0.0.0 ni 255.255.255.255${nc}"
+	fi 
+	
+	# Validar cada octeto entre 0 y 255
+    	IFS='.' read -r a b c d <<< "$ip"
+
+    	for octeto in $a $b $c $d; do
+        	if (( octeto < 0 || octeto > 255 )); then
+            	echo -e "${rojo}Direccion IP invalida, no puede ser mayor a 255 ni menor a 0${nc}"
+            	return 1
+        	fi
+    	done
+
+    	return 0
+}  
+
+bucle_Verificacion(){
+	msg=("Rango inicial de la IP:",  "Rango final de la IP:")
+	if [ "$opc" -e 1]; then
+		
+	fi else
 }
 
-# Entregable 1
-#Comprobar si esta instalada la paqueteria de DHCP
-echo -e "${amarillo}Verificando paqueteria DHCP...${nc}"
-if ! zypper search --installed-only | grep -q dhcp; then
-	echo -e "${rojo}DHCP no esta instalado, instalando...${nc}"
-	sudo zypper install dhcp-server
+verificar_Instalacion(){
+	# Entregable 1
+	# Comprobar si esta instalada la paqueteria de DHCP
+	
+	echo -e "${amarillo}Verificando paqueteria DHCP...${nc}"
 	if ! zypper search --installed-only | grep -q dhcp; then
-		echo -e "${rojo}Tiene que instalar dhcp para poder continuar, vuelvalo a intentar...${nc}"
-		exit
+		echo -e "${rojo}DHCP no esta instalado${nc}"
+		read -p "Desea instalarlo?" opc
+		if ! zypper search --installed-only | grep -q dhcp; then
+			echo -e "${rojo}Tiene que instalar dhcp para poder continuar, vuelvalo a intentar...${nc}"
+			exit
+		fi
 	fi
-fi
-echo -e "${verde}DHCP esta instalado, continuando...${nc}"
- 
-# Entregable 2
-echo -e "\nConfiguracion Dinamica\n"
-read -p "Nombre descriptivo del Ambito: " nombre_IP
-until
-	read -p "Rango inicial de la IP: " ip_Inicial
-	ip_Correcta "$ip_Inicial"
-do
-	echo -e "Intentando nuevamente..."
-done
-until	
-	read -p "Rango final de la IP:" ip_Final
-	ip_Correcta "$ip_Final"
-do
-	echo -e "Intendo nuevamente..."
-done
-read -p "Tiempo de la sesion: " tiempo_Sesion
-until
-	read -p "Gateway: " gateway
-	ip_Correcta "$gateway"
-do
-	echo -e "Intentado nuevamente..."
-done
-until	
-	read -p "DNS: " dns
-	ip_Correcta "$dns"
-do
-	echo -e "Intentado nuevamente..."
-done
+}
+
+instalar_DHCP(){
+	sudo zypper --non-interactive install dhcp-server
+	echo -e "${verde}DHCP esta instalado, continuando con la configuracion...${nc}"
+}
+
+configurar_DHCP(){
+	# Entregable 2
+	local ip_Valida="no"
+
+	echo -e "\nConfiguracion Dinamica\n"
+
+	read -p "Nombre descriptivo del Ambito: " nombre_IP
+	until
+		read -p "Rango inicial de la IP: " ip_Inicial && \
+		validar_IP "$ip_Inicial"
+	do
+		echo -e "Intentando nuevamente..."
+	done
+
+	until  [ "$ip_Valida" = "si" ]; do
+		read -p "Rango final de la IP:" ip_Final
+		if validar_IP "$ip_Final" then
+			if [ $(echo "$ip_Final" | cut -d'.' -f4) -gt $(echo "$ip_Inicio" | cut -d'.' -f4) ] && \
+			   [ $(echo "$ip_Final" | cut -d'.' -f1-3) -e $(echo "$ip_Inicio" | cut -d'.' -f1-3) ]; then
+				ip_Valida="si"
+			else
+				echo -e "${rojo}La IP no concuerda con el rango incial${nc}"
+			fi
+		fi
+		
+		if "$ip_Valida" = "no"; then
+			echo -e "Intendo nuevamente..."
+		fi
+	done
+
+	read -p "Tiempo de la sesion: " tiempo_Sesion
+
+	until
+		read -p "Gateway: " gateway && \
+		validar_IP "$gateway"
+	do
+		echo -e "Intentado nuevamente..."
+	done
+	until	
+		read -p "DNS: " dns && \
+		validar_IP "$dns"
+	do
+		echo -e "Intentado nuevamente..."
+	done
+}
+
+configurar_DHCP
+
